4b68ae49568dba585b0bfe95faa002573a55de1c
Bruno Khelifi <khelifi@khelifi@apc.in2p3.fr>
2021-09-22 19:12:48 +0200

    Differentiate the different messages and propagate them

--- gammapy/estimators/parameter.py
+++ gammapy/estimators/parameter.py
@@ -7,6 +7,13 @@ from .core import Estimator
 
 log = logging.getLogger(__name__)
 
+AVAILABLE_METHODS = [
+    "ts",
+    "err",
+    "ul",
+    "scan"
+]
+
 
 class ParameterEstimator(Estimator):
     """Model parameter estimator.
@@ -36,6 +43,8 @@ class ParameterEstimator(Estimator):
         Fit instance specifying the backend and fit options.
     reoptimize : bool
         Re-optimize other free model parameters. Default is True.
+    full_output: bool
+        Print the messages of all fitters. Default is False
     """
 
     tag = "ParameterEstimator"
@@ -49,11 +58,13 @@ class ParameterEstimator(Estimator):
         selection_optional=None,
         fit=None,
         reoptimize=True,
+        full_output=False, #BKH
     ):
         self.n_sigma = n_sigma
         self.n_sigma_ul = n_sigma_ul
         self.null_value = null_value
         self.selection_optional = selection_optional
+        self.full_output = full_output
 
         if fit is None:
             fit = Fit()
@@ -78,25 +89,33 @@ class ParameterEstimator(Estimator):
 
                 * parameter.name: best fit parameter value
                 * "stat": best fit total stat.
-                * "success": boolean flag for fit success
                 * parameter.name_err: covariance-based error estimate on parameter value
+                * "success": boolean flag for fit success
+                * "message": message of the fitter
+                * "ncalls": Number of minimizer calls
         """
         value, total_stat, success, error = np.nan, 0.0, False, np.nan
         # value, total_stat, success, message, error = np.nan, 0, False, "", np.nan
+        # value, total_stat, success, message, error, ncalls = np.nan, 0, False, "", np.nan, 0
 
         if np.any(datasets.contributes_to_stat):
             result = self.fit.run(datasets=datasets)
             value, error = parameter.value, parameter.error
             total_stat = result.optimize_result.total_stat
             success = result.success
+            # # message = result["optimize_result"].message BKH
+            # total_stat = result["optimize_result"].total_stat
+            # success = result["optimize_result"].success
             # message = result["optimize_result"].message
+            # ncalls = result["optimize_result"].ncalls
 
         return {
             f"{parameter.name}": value,
             "stat": total_stat,
-            "success": success,
-            # "message": message,
             f"{parameter.name}_err": error * self.n_sigma,
+            "success": success,
+            "message": message,
+            "ncalls": ncalls,
         }
 
     def estimate_ts(self, datasets, parameter):
@@ -116,26 +135,30 @@ class ParameterEstimator(Estimator):
 
                 * "ts" : fit statistic difference with null hypothesis
                 * "npred" : predicted number of counts per dataset
+                # * "npred_null" : predicted number of counts per dataset in the null hypothesis BKH
+                # * "success_ts": boolean flag for fit success
+                # * "message_ts": message of the fitter
         """
         npred = self.estimate_npred(datasets=datasets)
-        success = True
+        success, message = True, ""
 
         if not np.any(datasets.contributes_to_stat):
             stat = np.nan
             npred["npred"][...] = np.nan
             success = False
+            message = "Dataset not contributing"
         else:
             stat = datasets.stat_sum()
+            message = "Succeed to evaluate the TS"
 
         with datasets.parameters.restore_status():
             # compute ts value
             parameter.value = self.null_value
 
-            # message, success = "Not Applicable", True
             if self.reoptimize:
                 parameter.frozen = True
                 res = self.fit.optimize(datasets=datasets)
-                # message = res.message
+                message = res.message
                 success &= res.success
 
             ts = datasets.stat_sum() - stat
@@ -144,8 +167,8 @@ class ParameterEstimator(Estimator):
             "ts": ts,
             "npred": npred["npred"],
             "npred_null": npred_null["npred"],
-            "success_err": success,
-            # "message_ts": message,
+            "success_ts": success,
+            "message_ts": message,
         }
 
     def estimate_errn_errp(self, datasets, parameter):
@@ -165,11 +188,15 @@ class ParameterEstimator(Estimator):
 
                 * {parameter.name}_errp : positive error on parameter value
                 * {parameter.name}_errn : negative error on parameter value
+                # * "success_err": boolean flag for error success BKH
+                # * "message_err": message of the fitter
         """
         if not np.any(datasets.contributes_to_stat):
             return {
                 f"{parameter.name}_errp": np.nan,
                 f"{parameter.name}_errn": np.nan,
+                "success_err": False,
+                "message_err": "Dataset not contributing",
             }
 
         self.fit.optimize(datasets=datasets)
@@ -180,10 +207,17 @@ class ParameterEstimator(Estimator):
             sigma=self.n_sigma,
             reoptimize=self.reoptimize,
         )
+
+        message = ""
+        for k, msg in res.items():
+            if k.startswith('message'):
+                message += msg
+
         return {
             f"{parameter.name}_errp": res["errp"],
             f"{parameter.name}_errn": res["errn"],
             "success_err": res["fit_results"],
+            "message_err": message,
         }
 
     def estimate_scan(self, datasets, parameter):
@@ -203,6 +237,8 @@ class ParameterEstimator(Estimator):
 
                 * parameter.name_scan : parameter values scan
                 * "stat_scan" : fit statistic values scan
+                * "success_scan": boolean flag for scan success
+                * "message_scan": message of the fitter
         """
         scan_values = parameter.scan_values
 
@@ -210,6 +246,8 @@ class ParameterEstimator(Estimator):
             return {
                 f"{parameter.name}_scan": scan_values,
                 "stat_scan": scan_values * np.nan,
+                "success_scan": False, #BKH
+                "message_scan": "Dataset not contributing",
             }
 
         self.fit.optimize(datasets=datasets)
@@ -217,17 +255,16 @@ class ParameterEstimator(Estimator):
         profile = self.fit.stat_profile(
             datasets=datasets, parameter=parameter, reoptimize=self.reoptimize
         )
-        success = True
-        for res in profile["fit_results"]:
+        success, message = True, ""
+        for res, mes in zip(profile["fit_results"], profile["message_prof"]):
             success &= res
-            if res is False:
-                log.warning(f"Failed to make the parameter scan for {parameter.name}")
+            message += mes
 
         return {
             f"{parameter.name}_scan": scan_values,
             "stat_scan": profile["stat_scan"],
-            "success_err": success,
-            "success_scan": profile["fit_results"],
+            "success_scan": success,
+            "message_scan": message,
         }
 
     def estimate_ul(self, datasets, parameter):
@@ -246,9 +283,15 @@ class ParameterEstimator(Estimator):
             Dict with the parameter ULs. Entries are:
 
                 * parameter.name_ul : upper limit on parameter value
+                * "success_ul": boolean flag for upper limit success
+                * "message_ul": message of the fitter
         """
         if not np.any(datasets.contributes_to_stat):
-            return {f"{parameter.name}_ul": np.nan}
+            return {
+                f"{parameter.name}_ul": np.nan,
+                "success_ul": False,
+                "message_ul": "Dataset not contributing",
+            }
 
         self.fit.optimize(datasets=datasets)
 
@@ -259,9 +302,15 @@ class ParameterEstimator(Estimator):
             reoptimize=self.reoptimize,
         )
 
+        message = ""
+        for k, msg in res.items():
+            if k.startswith('message'):
+                message += msg
+
         return {
             f"{parameter.name}_ul": res["errp"] + parameter.value,
-            "success_err": res["fit_results"],
+            "success_ul": res["fit_results"],
+            "message_ul": message,
         }
 
     @staticmethod
@@ -342,31 +391,37 @@ class ParameterEstimator(Estimator):
                 parameter.frozen = False
 
             result.update(self.estimate_best_fit(datasets, parameter))
+            result["fit_status"] = int(result["success"])
+
             result.update(self.estimate_ts(datasets, parameter))
 
+            # If the fit has not converged, should we compute the other quantities?
+            other_success = True
             if "errn-errp" in self.selection_optional:
                 result.update(self.estimate_errn_errp(datasets, parameter))
+                other_success &= result["success_err"]
 
             if "ul" in self.selection_optional:
                 result.update(self.estimate_ul(datasets, parameter))
+                other_success &= result["success_ul"]
 
             if "scan" in self.selection_optional:
                 result.update(self.estimate_scan(datasets, parameter))
+                other_success &= result["success_scan"]
+
+        if result["fit_status"] == 1 and other_success:
+            result["fit_status"] = 2
 
         result.update(self.estimate_counts(datasets))
 
-        result["fit_status"] = int(result["success"])
-        if result["fit_status"] > 0 and "success_err" in result:
-            result["fit_status"] += int(result["success_err"])
-            if "scan" in self.selection_optional and "success_scan" in result:
-                if False in result["success_scan"]:
-                    result["fit_status"] = 1
-        if "success_err" in result:
-            del result["success_err"]
-        if "success_scan" in result:
-            del result["success_scan"]
+        if self.full_output:
+            log.info(result["message"])
+        for method in AVAILABLE_METHODS:
+            if f"message_{method}" not in result:
+                continue
+            if not self.full_output:
+                del result[f"success_{method}"], result[f"message_{method}"]
+            else:
+                log.info(result[f"message_{method}"])
 
-        # Temporary DEBUG for this draft
-        if result["fit_status"] > 2:
-            print("\nBKH> LOGIC ERROR fit_status=", result["fit_status"])
         return result
